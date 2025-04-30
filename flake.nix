a52e08740cc2c0437803e31e32d704e65027265c
Loi Chyan <loichyan@foxmail.com>
2025-04-30 17:12:52 +0800

    build: add GNU toolchain for riscv64-none-elf

--- flake.nix
+++ flake.nix
@@ -24,15 +24,41 @@
           inherit system;
           overlays = [ rust-overlay.overlays.default ];
         };
-        inherit (pkgs) mkShell rust-bin;
-        qemu-7_0_0 = nixpkgs-qemu-7_0_0.legacyPackages.${system}.qemu;
+        inherit (pkgs)
+          fetchurl
+          lib
+          mkShell
+          rust-bin
+          stdenv
+          ;
+
         rust-dev = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        qemu-7_0_0 = nixpkgs-qemu-7_0_0.legacyPackages.${system}.qemu;
+        # Use a prebuilt gnu toolchain, since nixpkgs does not support riscv64-embedded.
+        riscv-embedded-toolchain = lib.optionals (system == "x86_64-linux") (
+          stdenv.mkDerivation rec {
+            pname = "riscv-embedded-toolchain";
+            version = "14.2.0-3";
+            src = fetchurl {
+              url = "https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack/releases/download/v${version}/xpack-riscv-none-elf-gcc-${version}-linux-x64.tar.gz";
+              hash = "sha256-9XRBW2PxKwm900dSI6tJKkZdI4EGRskME6TDtnbINQM=";
+            };
+            nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+            sourceRoot = "xpack-riscv-none-elf-gcc-${version}";
+            installPhase = ''
+              runHook preInstall
+              cp -r . $out
+              runHook postInstall
+            '';
+          }
+        );
       in
       {
         devShells.default = mkShell {
           packages = [
             pkgs.cargo-binutils
             qemu-7_0_0
+            riscv-embedded-toolchain
             (rust-dev.override (prev: {
               extensions = prev.extensions ++ [ "rust-analyzer" ];
             }))
