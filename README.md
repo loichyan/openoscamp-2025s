8fefd284619a6182a0ae4086b7cb9a95b5152605
Loi Chyan <loichyan@foxmail.com>
2025-05-24 17:04:48 +0800

    build: prepare development environment

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🏕️ 2025 春夏季开源操作系统训练营
+
+这里记录了 2025 春夏季开源操作系统训练营[^1]的学习成果．
+
+## ⚖️ 许可
+
+本仓库所包含之代码及文档，如无特殊说明，均以下列任意许可授权：
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+
+[^1]: <https://opencamp.cn/os2edu/camp/2025spring>
---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        inherit (pkgs) lib mkShellNoCC rust-bin;
+
+        rustupToolchain = (lib.importTOML ./rust-toolchain.toml).toolchain;
+        crateMetadata = (lib.importTOML ./Cargo.toml).package;
+
+        # Rust toolchain for development
+        rust-dev = rust-bin.fromRustupToolchain rustupToolchain;
+        rust-dev-with-rust-analyzer = rust-dev.override (prev: {
+          extensions = prev.extensions ++ [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        });
+
+        # Rust toolchain of MSRV
+        rust-msrv = rust-bin.fromRustupToolchain {
+          channel = crateMetadata.rust-version;
+          profile = "minimal";
+        };
+
+        mkDevShell = devPkgs: (mkShellNoCC { packages = devPkgs; });
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = crateMetadata.name;
+          version = crateMetadata.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          meta = {
+            description = crateMetadata.description;
+            homepage = crateMetadata.repository;
+            license = with lib.licenses; [
+              mit
+              asl20
+            ];
+          };
+        };
+
+        # The default devShell with IDE integrations
+        devShells.default = mkDevShell [ rust-dev-with-rust-analyzer ];
+        # A minimal devShell with toolchain of MSRV
+        devShells.msrv = mkDevShell [ rust-msrv ];
+      }
+    );
+}
