7786c5b4caef2728f24e61e6a5b6765507022a44
Loi Chyan <loichyan@foxmail.com>
2025-06-20 16:47:42 +0800

    test(evering-ipc): add benchmark for socket+epoll

--- examples/evering-ipc/benches/ping_pong.rs
+++ examples/evering-ipc/benches/ping_pong.rs
@@ -17,12 +17,15 @@
 use std::ffi::CString;
 use std::mem::MaybeUninit;
 use std::os::fd::AsFd;
+use std::path::Path;
+use std::sync::Once;
 use std::time::{Duration, Instant};
 
 use bytesize::ByteSize;
 use criterion::{Criterion, criterion_group, criterion_main};
+use tokio::task::spawn_local;
 
-const BUFSIZES: &[u64] = &[
+const BUFSIZES: &[usize] = &[
     64,
     512,
     1024,
@@ -37,40 +40,47 @@ const BUFSIZES: &[u64] = &[
 const CONCURRENCY: usize = 200;
 const SHMSIZE: usize = 1 << 30;
 
-fn bench_evering(id: &str, iters: u64, bufsize: u64) -> Duration {
+fn block_on<T>(fut: impl Future<Output = T>) -> T {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    tokio::task::LocalSet::new().block_on(&rt, fut)
+}
+
+fn make_shmid(pref: &str) -> String {
+    pref.chars()
+        .chain(std::iter::repeat_with(fastrand::alphanumeric).take(6))
+        .collect()
+}
+
+fn bench_evering(id: &str, iters: usize, bufsize: usize) -> Duration {
     use evering::uring::Uring;
     use evering_ipc::*;
 
-    let bufsize = usize::try_from(bufsize).unwrap();
-    let shmid = id
-        .chars()
-        .chain(std::iter::repeat_with(fastrand::alphanumeric).take(6))
-        .collect::<String>();
+    let shmid = make_shmid(id);
     let shmid = CString::new(shmid).unwrap();
-    let shmfd_owned = {
+    let shmfd_owned =
         nix::sys::memfd::memfd_create(shmid.as_c_str(), nix::sys::memfd::MFdFlags::empty())
-            .expect("failed to create shared memory")
-    };
-
+            .expect("failed to create shared memory");
     let shmfd = shmfd_owned.as_fd();
-    let shm = unsafe {
-        let mut h = UringBuilder::new();
-        h.size_a(CONCURRENCY.next_power_of_two());
-        h.size_b(CONCURRENCY.next_power_of_two());
-        let h = h.build_header();
-        ShmHeader::create(shmfd, SHMSIZE, h).unwrap()
-    };
 
     let mut elapsed = Duration::ZERO;
+    let signal = Once::new();
     std::thread::scope(|cx| {
         // Server
         cx.spawn(|| {
             let (shm, mut rq);
             unsafe {
-                shm = ShmHeader::open(shmfd, SHMSIZE).unwrap();
+                let mut h = UringBuilder::new();
+                h.size_a(CONCURRENCY.next_power_of_two());
+                h.size_b(CONCURRENCY.next_power_of_two());
+                let h = h.build_header();
+                shm = ShmHeader::create(shmfd, SHMSIZE, h).unwrap();
                 rq = ServerUring::from_raw(shm.as_ref().build_raw_uring());
                 evering_ipc::shm::init_server(shm.as_ref());
             }
+            signal.call_once(|| {});
 
             let mut pending = None::<Rqe>;
             'outer: loop {
@@ -108,9 +118,9 @@ fn bench_evering(id: &str, iters: u64, bufsize: u64) -> Duration {
             _ = rq.dispose_raw();
             unsafe { ShmHeader::close(shm, SHMSIZE).unwrap() }
         });
-
         // Client
         cx.spawn(|| {
+            signal.wait();
             let (shm, sq);
             unsafe {
                 shm = ShmHeader::open(shmfd, SHMSIZE).unwrap();
@@ -118,46 +128,133 @@ fn bench_evering(id: &str, iters: u64, bufsize: u64) -> Duration {
                 evering_ipc::shm::init_client(shm.as_ref());
             }
 
-            let rt = evering_ipc::Runtime::new(sq);
-            rt.block_on(async {
+            let rx = evering_ipc::Runtime::new(sq);
+            block_on(rx.run_on(async {
                 let tasks = std::iter::repeat_with(|| async move {
                     let mut rbuf = ShmBox::new_uninit_slice(bufsize);
-                    for _ in 0..(iters as usize / CONCURRENCY) {
+                    for _ in 0..(iters / CONCURRENCY) {
                         let rbuf_init = evering_ipc::op::ping(Duration::ZERO, rbuf).await;
                         assert!(rbuf_init.iter().all(|b| *b == 0));
                         rbuf = rbuf_init.into_uninit();
                     }
                 })
-                .map(RuntimeHandle::spawn)
+                .map(spawn_local)
                 .take(CONCURRENCY)
                 .collect::<Vec<_>>();
 
                 let now = Instant::now();
                 for task in tasks {
-                    task.await;
+                    task.await.unwrap();
                 }
                 elapsed = now.elapsed();
                 evering_ipc::op::exit().await;
-            });
+            }));
 
-            _ = rt.into_uring().dispose_raw();
+            _ = rx.into_uring().dispose_raw();
             unsafe { ShmHeader::close(shm, SHMSIZE).unwrap() }
         });
     });
 
-    unsafe { ShmHeader::close(shm, SHMSIZE).unwrap() }
     nix::unistd::close(shmfd_owned).expect("failed to remove shared memory");
     elapsed
 }
 
+fn bench_epoll(id: &str, iters: usize, bufsize: usize) -> Duration {
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    use tokio::net::{UnixListener, UnixStream};
+
+    const PING: i32 = 1;
+    const PONG: i32 = 1;
+
+    let sock = Path::new("/dev/shm").join(make_shmid(id));
+
+    let mut elapsed = Duration::ZERO;
+    let (signal_tx, signal_rx) = tokio::sync::oneshot::channel::<()>();
+    let (exit_tx, mut exit_rx) = tokio::sync::oneshot::channel::<()>();
+    std::thread::scope(|cx| {
+        // Server
+        cx.spawn(|| {
+            block_on(async {
+                let listener = UnixListener::bind(&sock).unwrap();
+                signal_tx.send(()).unwrap();
+                let worker = |mut conn: UnixStream| async move {
+                    let wbuf = vec![0; bufsize];
+                    loop {
+                        match conn.read_i32().await {
+                            Ok(i) => {
+                                assert_eq!(i, PING);
+                                conn.write_i32(PONG).await.unwrap();
+                                conn.write_all(&wbuf).await.unwrap();
+                                conn.flush().await.unwrap();
+                            },
+                            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
+                                return;
+                            },
+                            Err(e) => panic!("{e}"),
+                        }
+                    }
+                };
+                loop {
+                    tokio::select! {
+                        r = listener.accept() => {
+                            let (conn, _) = r.unwrap();
+                            spawn_local(worker(conn));
+                        },
+                        _ = &mut exit_rx =>  break,
+                    }
+                }
+            });
+        });
+        // Client
+        cx.spawn(|| {
+            block_on(async {
+                signal_rx.await.unwrap();
+                let tasks = std::iter::repeat_with(|| {
+                    let sock = sock.clone();
+                    async move {
+                        let mut conn = UnixStream::connect(sock).await.unwrap();
+                        let mut rbuf = vec![0; bufsize];
+                        for _ in 0..(iters / CONCURRENCY) {
+                            conn.write_i32(PING).await.unwrap();
+                            conn.flush().await.unwrap();
+                            assert_eq!(conn.read_i32().await.unwrap(), PONG);
+                            conn.read_exact(&mut rbuf).await.unwrap();
+                            assert!(rbuf.iter().all(|b| *b == 0));
+                        }
+                    }
+                })
+                .map(spawn_local)
+                .take(CONCURRENCY)
+                .collect::<Vec<_>>();
+
+                let now = Instant::now();
+                for task in tasks {
+                    task.await.unwrap();
+                }
+                elapsed = now.elapsed();
+                exit_tx.send(()).unwrap();
+            });
+        });
+    });
+
+    _ = std::fs::remove_file(sock);
+    elapsed
+}
+
 fn groups(c: &mut Criterion) {
+    type BenchFn = fn(&str, usize, usize) -> Duration;
     let mut g = c.benchmark_group("evering");
     for (i, bufsize) in BUFSIZES.iter().copied().enumerate() {
-        let bsize = ByteSize::b(bufsize).display().iec_short();
-        let id = format!("ping_pong_evering_{:02}_{bsize:.0}", i + 1);
-        g.bench_function(&id, |b| {
-            b.iter_custom(|iters| bench_evering(&id, iters, bufsize))
-        });
+        let bsize = ByteSize::b(bufsize as u64).display().iec_short();
+        for (name, f) in [
+            ("evering", bench_evering as BenchFn),
+            ("epoll", bench_epoll as BenchFn),
+        ] {
+            let id = format!("ping_pong_{name}_{:02}_{bsize:.0}", i + 1);
+            g.bench_function(&id, |b| {
+                b.iter_custom(|iters| f(&id, iters as usize, bufsize))
+            });
+        }
     }
 }
 
