6879e93a752eebd9561aeb5c5b83bcda0de0ebd3
Loi Chyan <loichyan@foxmail.com>
2025-06-21 13:18:43 +0800

    build: prepare Python environment

--- flake.nix
+++ flake.nix
@@ -33,13 +33,27 @@
             "rust-analyzer"
           ];
         });
+
+        libPath = lib.makeLibraryPath (
+          with pkgs;
+          [
+            # Required by numpy
+            stdenv.cc.cc
+            zlib
+          ]
+        );
       in
       {
         devShells.default = mkShellNoCC {
           packages = with pkgs; [
             rust-dev-with-rust-analyzer
             gnuplot
+            python312
+            uv
           ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${libPath}
+          '';
         };
       }
     );
